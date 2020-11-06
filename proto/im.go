a5424641229ce826dd1a1227075000fc9337d174
xuyang <y.xu@neoclub.cn>
2020-11-06 13:59:40 +0800

    feat:消息协议定义好了

--- proto/im.go
+++ proto/im.go
@@ -11,22 +11,19 @@ type NimProtocol struct {
 type CmdEnum uint8
 
 const (
-	Ping CmdEnum = 1
-	Pong CmdEnum = 2
-	Ack  CmdEnum = 3 //消息ACK
-	Message  CmdEnum = 4 //消息
-	//ChatRoom   CmdEnum = 4  //聊天室
-	//GroupChat  CmdEnum = 5  //群聊
-	//SingleChat CmdEnum = 6  //单聊
-	AuthReq    CmdEnum = 7  //认证请求
-	AuthRes    CmdEnum = 8  //认证结果
-	LogoutReq  CmdEnum = 9  //退出请求
-	LogoutRes  CmdEnum = 10 //退出结果
-	KickoutReq CmdEnum = 11 //踢人
-	KickoutRes CmdEnum = 12
-	PullMsgReq CmdEnum = 13 //拉取消息
-	PullMsgRes CmdEnum = 14
-	PushMsgReq CmdEnum = 15 //推送消息
-	PushMsgRes CmdEnum = 16
-	Notify     CmdEnum = 17 //通知类消息 下发通知、派对被关闭、帖子被删等消息
+	Ping           CmdEnum = 1
+	Pong           CmdEnum = 2
+	AuthReq        CmdEnum = 3 //认证请求
+	AuthRes        CmdEnum = 4 //认证结果
+	LogoutReq      CmdEnum = 5 //退出请求
+	LogoutRes      CmdEnum = 6 //退出结果
+	KickoutReq     CmdEnum = 7 //踢人
+	KickoutRes     CmdEnum = 8
+	PullMsgReq     CmdEnum = 9 //拉取消息
+	PullMsgRes     CmdEnum = 10
+	MessageSend    CmdEnum = 11 //消息
+	MessageSendAck CmdEnum = 12 //发送消息ACK
+	MessageNotify  CmdEnum = 13 //下发聊天消息
+	Notify         CmdEnum = 14 //通知类消息 下发通知、派对被关闭、帖子被删等消息
+	NotifyAck      CmdEnum = 15 // 通知类的ACK
 )
